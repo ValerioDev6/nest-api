58bc1fef7fa1718e0281262a28edf751020c08b1
Principe Mestizo <vquispealarcon@gmail.com>
2024-10-15 00:01:41 -0500

    backend avance icase-store

--- src/telefono-personal/telefono-personal.service.ts
+++ src/telefono-personal/telefono-personal.service.ts
@@ -1,14 +1,10 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateTelefonoPersonalDto } from './dto/create-telefono-personal.dto';
 
 @Injectable()
 export class TelefonoPersonalService {
   constructor(private readonly prisma: PrismaService) {}
   private readonly logger = new Logger('TipoZonaService');
-  create(createTelefonoPersonalDto: CreateTelefonoPersonalDto) {
-    return 'This action adds a new telefonoPersonal';
-  }
 
   findAll() {
     return `This action returns all telefonoPersonal`;
